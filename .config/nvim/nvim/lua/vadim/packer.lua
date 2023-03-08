96636aec04f1780c511ce76f16c6866875e10c72
Rcf47 <40764481+Rcf47@users.noreply.github.com>
2023-03-08 20:36:49 +0700

    Add files via upload
    My neovim dotfiles

---
+++ .config/nvim/nvim/lua/vadim/packer.lua
@@ -0,0 +1,83 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'akinsho/nvim-bufferline.lua'
+
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+
+
+  use({
+    --keyboard layout
+    "yorik1984/lualine-xkblayout",
+    requires = {
+      "nvim-lualine/lualine.nvim",
+      "lyokha/vim-xkbswitch",
+    },
+  })
+
+
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+
+  use 'nvim-telescope/telescope-file-browser.nvim'
+
+
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use 'ThePrimeagen/harpoon'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
+      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    }
+  }
+  use 'williamboman/mason.nvim'
+
+  use('prettier/vim-prettier', { run = 'npm install --frozen-lockfile --production' })
+
+  use "lukas-reineke/lsp-format.nvim"
+  use "tpope/vim-surround"
+
+  --Debug adapter protocol
+  use 'mfussenegger/nvim-dap'
+  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+  use "folke/neodev.nvim" --for dap-ui
+  use { "mxsdev/nvim-dap-vscode-js", requires = { "mfussenegger/nvim-dap" } }
+  use {
+    "microsoft/vscode-js-debug",
+    opt = true,
+    run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+  }
+end)
---
+++ .config/nvim/nvim/lua/vadim/remap.lua
@@ -0,0 +1,86 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>vwm", function()
+  require("vim-with-me").StartVimWithMe()
+end)
+vim.keymap.set("n", "<leader>svwm", function()
+  require("vim-with-me").StopVimWithMe()
+end)
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s:\<<C-r><C-w>\>:<C-r><C-w>:gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/vadim/packer.lua<CR>");
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+vim.keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+
+--From craftzdog maps.lua
+-- Select all
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+
+-- New tab
+vim.keymap.set('n', 'te', ':tabedit<cr>')
+-- Split window
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+
+--Insert mode
+vim.keymap.set('i', '<a-l>', '<right>')
+vim.keymap.set('i', '<a-h>', '<left>')
+vim.keymap.set('i', '<a-j>', '<down>')
+vim.keymap.set('i', '<a-k>', '<up>')
+--Save
+vim.keymap.set({ "i", "n" }, "<a-s>", "<cmd>wa<CR><cmd>echo 'Save'<CR> ", { desc = "Save all" })
+
+--:buffers
+vim.keymap.set('n', '<leader>b', "<cmd>buffers<CR>")
+
+--:nohlsearch
+vim.keymap.set('n', '<leader>h', '<cmd>nohlsearch<CR>')
+
+--debugging
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F6>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F7>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F8>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>tb', function() require('dap').toggle_breakpoint() end)
+
+--debugging dap-ui
+vim.keymap.set('n', '<leader>o', "<cmd>lua require('dapui').toggle() <CR>")
