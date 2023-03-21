f3bdd9650c24e00e4d1672d3b62935309fb1a825
Rcf47 <40764481+Rcf47@users.noreply.github.com>
2023-03-21 22:46:04 +0700

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+ if status is-interactive                                      
+alias ncal="ncal -b"                                          
+alias shtd="sudo shutdown now"                                
+export EDITOR=/usr/bin/nvim                                   
+#alt+f for exact find                                         
+bind \ef ~/Programms/scripts/findWithFzf.sh                   
+#alt+d just find                                              
+bind \ed ~/Programms/scripts/findWithFzfnotExact.sh           
+end                                                           
+                                                              
