96dfecb4405b2d6638ac2b0e127e45486a2866e6
Vadim Cherepanov <xenos26@gmail.com>
2023-03-24 18:47:19 +0700

    fix: delete fish_config tmux_config

--- fish_config/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-alias ncal="ncal -b"
-alias shtd="sudo shutdown now"
-export EDITOR=/usr/bin/nvim
-#alt+f for exact find
-bind \ef ~/Programms/scripts/findWithFzf.sh
-#alt+d just find
-bind \ed ~/Programms/scripts/findWithFzfnotExact.sh
-end
