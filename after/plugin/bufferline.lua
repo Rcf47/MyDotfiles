8c4f93712d90e49ac1467a4d446699d001a311ce
Vadim Cherepanov <xenos26@gmail.com>
2023-04-19 11:25:14 +0700

    docs: add shortcuts for bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -4,11 +4,12 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
-    always_show_bufferline = false,
+    numbers = "ordinal",
+    separator_style = 'thin',
+    always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
   },
   highlights = {
     separator = {
@@ -34,3 +35,13 @@ bufferline.setup({
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+
+vim.keymap.set('n', '<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>', {})
+vim.keymap.set('n', '<leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>', {})
+vim.keymap.set('n', '<leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>', {})
+vim.keymap.set('n', '<leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>', {})
+vim.keymap.set('n', '<leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>', {})
+vim.keymap.set('n', '<leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>', {})
+vim.keymap.set('n', '<leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>', {})
+vim.keymap.set('n', '<leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>', {})
+vim.keymap.set('n', '<leader>9', '<Cmd>BufferLineGoToBuffer -1<CR>', {})
