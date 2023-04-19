b1c78ebfd1454ded4a990e077d6d84d83f96cc50
Vadim Cherepanov <xenos26@gmail.com>
2023-04-19 12:54:11 +0700

    docs: toggle always show tabs

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -6,7 +6,7 @@ bufferline.setup({
     mode = "tabs",
     numbers = "ordinal",
     separator_style = 'thin',
-    always_show_bufferline = true,
+    always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true,
