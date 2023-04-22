56fb0859a8f37e80bd52f08592e4cf04bb5490c8
Vadim Cherepanov <xenos26@gmail.com>
2023-04-22 11:18:40 +0700

    docs: add Diffview description in which key

--- after/plugin/whichkey.lua
+++ after/plugin/whichkey.lua
@@ -1,10 +1,11 @@
 local wk = require("which-key")
 
 wk.register({
-      ["<leader>t"] = { name = "+Telescope" },
-      ["<leader>g"] = { name = "+Fugitive git" },
-      ["<leader>h"] = { name = "+Gitsigns" },
-      ["s"] = { name = "+Split window" },
-      ["<leader><leader>"] = { name = "+Highlight color" },
-      ["<leader>l"] = { name = "+Lazygit" },
+  ["<leader>t"] = { name = "+Telescope" },
+  ["<leader>g"] = { name = "+Fugitive git" },
+  ["<leader>h"] = { name = "+Gitsigns" },
+  ["s"] = { name = "+Split window" },
+  ["<leader><leader>"] = { name = "+Highlight color" },
+  ["<leader>l"] = { name = "+Lazygit" },
+  ["<leader>d"] = { name = "+Diffview" },
 })
