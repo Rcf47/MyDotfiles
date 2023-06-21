19525679461e8e217df89c038cadacbbaa68a20a
Vadim Cherepanov <xenos26@gmail.com>
2023-06-22 00:36:05 +0700

    chore: change my function in lsp-zero setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,12 +38,12 @@ local enable_format_on_save = function(_, bufnr)
 end
 
 lsp.on_attach(function(client, bufnr)
-  local optsFunc = function(description) return {desc = description, buffer = bufnr, remap = false} end
+  local optsFunc = function(description) return { desc = description, buffer = bufnr, remap = false } end
   enable_format_on_save(client, bufnr)
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, optsFunc('Go definition'))
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, optsFunc('Hover code'))
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, optsFunc('Workspace Symbol'))
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, optsFunc('Diagnostic') )
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, optsFunc('Diagnostic'))
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, optsFunc('Go to next'))
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, optsFunc('Go to prev'))
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, optsFunc('Code action'))
