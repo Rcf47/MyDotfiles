48542c27cab07a10dc53589b9b8df0b9a544a085
Vadim Cherepanov <xenos26@gmail.com>
2023-08-04 03:49:21 +0700

    feat: add textwidth in init.lua

--- lua/vadim/set.lua
+++ lua/vadim/set.lua
@@ -23,7 +23,8 @@ vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.wrap = false         -- No Wrap lines
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.textwidth = 80
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
