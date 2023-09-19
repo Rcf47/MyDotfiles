23d245d028bf21a39592483697b4eb526f9fa3e0
Vadim Cherepanov <xenos26@gmail.com>
2023-09-19 20:30:07 +0700

    fix: fix null-ls eslint warning

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -7,7 +7,7 @@ local async = event == "BufWritePost"
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
+    --null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
   },
   on_attach = function(client, bufnr)
