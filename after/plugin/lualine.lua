962ef851413f13e33ebab054f33cb85217d18caf
Vadim Cherepanov <xenos26@gmail.com>
2024-02-10 22:32:13 +0700

    chore: lua prettier changes

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,59 +1,66 @@
 local status, lualine = pcall(require, "lualine")
-if (not status) then return end
+if not status then
+	return
+end
 
 vim.g.lualine_xkblayout_codes = {
-  ["2SetKorean"] = "KR",
-  ["Chinese"]    = "CN",
-  ["Japanese"]   = "JP",
-  ["us"]         = "ENG",
-  ["ru"]         = "РУС",
-  ["ua"]         = "УКР",
+	["2SetKorean"] = "KR",
+	["Chinese"] = "CN",
+	["Japanese"] = "JP",
+	["us"] = "ENG",
+	["ru"] = "РУС",
+	["ua"] = "УКР",
 }
-lualine.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
-    disabled_filetypes = {}
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
-    } },
-    lualine_x = {
-      {
-        'diagnostics',
-        sources = { "nvim_diagnostic" },
-        symbols = {
-          error = ' ',
-          warn = ' ',
-          info = ' ',
-          hint = ' '
-        }
-      },
-      'encoding',
-      'filetype'
-    },
-    lualine_y = { 'progress' },
-    lualine_z = { { 'xkblayout', icon = "" } }
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 2            -- 0 = just filename, 1 = relative path, 2 = absolute path
-    } },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = { 'fugitive' }
-}
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		disabled_filetypes = {},
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- displays file status (readonly status, modified status)
+				path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
+		},
+		lualine_x = {
+
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = {
+					error = " ",
+					warn = " ",
+					info = " ",
+					hint = " ",
+				},
+			},
+			"encoding",
+			"filetype",
+		},
+		lualine_y = { "progress" },
+		lualine_z = { { "xkblayout", icon = "" } },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- displays file status (readonly status, modified status)
+				path = 2, -- 0 = just filename, 1 = relative path, 2 = absolute path
+			},
+		},
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = { "fugitive" },
+})
